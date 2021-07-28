3137520dc4395289ccab1e39550d42ff6f6c44c8
Cristina Torres <ctorres@kiosolutions.com>
2021-07-27 20:10:34 -0400

    Base proyecto ejercicio gl.

---
+++ src/main/java/com/globallogic/ejerciciogl/EjercicioglApplication.java
@@ -0,0 +1,13 @@
+package com.globallogic.ejerciciogl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioglApplication.class, args);
+	}
+
+}
